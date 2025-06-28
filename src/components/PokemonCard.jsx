65783a1b2e39d05bd4fe5537a3ae9b3e6bfd5644
jordnlnn <80535966+jordnlnn@users.noreply.github.com>
2025-06-28 16:21:41 -0700

    deleted unnecessary components

--- src/components/PokemonCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function PokemonCard() {
-  return (
-    <div class="pokemon-container">
-      <img src="" class="pokemon-image" />
-    </div>
-  );
-}
--- src/components/PokemonNotFound.jsx
+++
@@ -1,7 +0,0 @@
-export default function PokemonNotFound() {
-  return (
-    <div class="not-found-error">
-      ‼️Oops! That Pokémon doesn't exist. Check your spelling and try again‼️
-    </div>
-  );
-}
